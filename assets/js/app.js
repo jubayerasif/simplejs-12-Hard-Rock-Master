1fb41ba9575c3fb719f8fd1278aad656de6c1d39
jubayerasif <jubaerasif101@gmail.com>
2023-03-09 19:31:07 +0600

    project completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ const searchSongs = () => {
   const url = `https://api.lyrics.ovh/suggest/${searchText}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displaySongs(data))
+    .then((data) => displaySongs(data.data))
     .catch((error) =>
       displayError("Something Went Wrong!! Please try again later")
     );
@@ -14,9 +14,8 @@ const displaySongs = (songs) => {
   songContainer.innerHTML = "";
   songs.forEach((song) => {
     const songDiv = document.createElement("div");
-    songDiv.className = "single result row align-items-center my-3 p-3";
     songDiv.innerHTML = `
-                <div class ="col-md-9">
+                <div class ="single-result row align-items-center my-3 p-3 col-md-9">
                     <h3 class="lyrics-name">${song.title}</h3>
                     <p class="author lead">Album by <span>${song.artist.name}</span></p>
                     <audio controls>
